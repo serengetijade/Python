92d20817c1bdbc28a33e932c2bfcca54431d2331
SerengetiJade <jad245@gmail.com>
2023-04-12 16:18:01 -0700

    Add project description.

--- Django/DjangoUniversity/Classes/campusApp/models.py
+++ Django/DjangoUniversity/Classes/campusApp/models.py
@@ -1,6 +1,11 @@
 from django.db import models
-
-#   This project is a small part of a larger project - it is a short excercize to create classes and define a model manager.
+#   Python Version: 3.10.8
+#
+#   Author:     Jade Abreu
+#
+#   Purpose:    This project is a small part of a larger project - it is a short excercize to create classes and define a model manager.
+#
+#   Tested OS:  Windows 11
 
 class UniversityCampus(models.Model):
     campus_id = models.IntegerField(primary_key=True)
